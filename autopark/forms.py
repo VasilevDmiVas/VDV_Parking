77b107a08bd4e264ed7dd67e408aea99a4a02c16
Дмитрий <vasja8102@mail.ru>
2024-02-04 22:17:31 +0300

    Добавлена таблица паркинг

--- autopark/forms.py
+++ autopark/forms.py
@@ -14,6 +14,4 @@ class CarTypeForm(forms.Form):
 class Car_all(forms.Form):
     car_number = forms.CharField(max_length=10, label='Введите гос номер авто')
     year = forms.IntegerField(label='Введите год выпуска авто')
-    # name = forms.CharField(max_length=100, label='Введите тип авто')
-    # car_type = forms.ModelChoiceField(queryset=CarType.objects.name, label='Выберите тип авто')
     is_electric = forms.BooleanField(label='Электропривод', required=False)
